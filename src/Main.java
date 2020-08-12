45b968827d9e18647cd5a913569c27a67672e3f2
John Blyatson <48108297+bleenMachine@users.noreply.github.com>
2020-08-12 15:45:59 -0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        NumberInWord.printNumberInWord(3);
+        NumberInWord.printNumberInWord(-7);
+        NumberInWord.printNumberInWord(11);
+        NumberInWord.printNumberInWord(0);
+        NumberInWord.printNumberInWord(8);
+    }
+}
